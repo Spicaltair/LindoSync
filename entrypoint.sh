e734381c2f1c8efdba762f9bf0f6784c5003f902
spicaltair-COM <y.kevin.lin@gmail.com>
2025-06-04 14:40:53 -0600

    clear cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "[INFO] Starting Playwright Publish..."
-python scripts/xhs_playwright.py
+
+echo "✅ Running playwright install..."
+playwright install --with-deps
+
+# 然后执行主逻辑
+exec gunicorn main:app
+
